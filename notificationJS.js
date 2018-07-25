5290cb83708f99e58014339624385aac0df091a4
max <maximogarciamtnez@gmail.com>
2018-07-25 11:51:37 +0200

    removed jquery dependency

--- notificationJS.js
+++ notificationJS.js
@@ -1,234 +1,251 @@
-if (!window.jQuery) {
-    var jQuery = document.createElement('script')
-    jQuery.src = "https://code.jquery.com/jquery-latest.min.js"
-    document.head.appendChild(jQuery)
-}
-
-
-
-checkErrors = function(notification) {
-    if (!notification.debug)
-        notification.debug = true
+checkErrors = function (notification) {
+    if (!notification.debug) notification.debug = true;
 
     if (!notification.title) {
-        notification.title = 'TITLE UNDEFINED'
-        if (notification.debug) {
-            console.warn("NotificationJS: You need to set a title.")
-        }
+        notification.title = 'TITLE UNDEFINED';
+        if (notification.debug)
+            console.warn('NotificationJS: You need to set a title.');
     }
 
     if (notification.link) {
         if (!notification.link.href)
-            if (notification.debug) {
-                console.error("NotificationJS: You need to set a link with href property")
-            }
+            if (notification.debug)
+                console.error(
+                    'NotificationJS: You need to set a link with href property'
+                );
     }
+    return notification;
+};
 
-
-    return notification
-
-}
-notficationJS_getDefaults = function(){
-    return {                   
-        status:"info",   
-        time:5,                                                 
-        hide:true,                                                
-        animation_duration:0.2,                                   
-        showCloseIcon: true,                                     
-        newestOnTop: true,                                       
-        debug: true                                    
-    }
-}
-notficationJS_getDefaultsStyles = function(){
+notficationJS_getDefaults = function () {
     return {
-        maxWidth:"375px",
-        backgroundColor:"#F1F5F6",
-        color:"#242424",
-        border:"none",
-        padding:"10px 15px",
-        borderRadius:"5px",
-        boxShadow:"0px 1px 9px 4px #242424",
-        marginTop:"15px",
-        width:"calc(100vw - 80px)",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between"
-    }
-}
-notficationJS_getDefaultsStylesTitle = function(){
+        status: 'info',
+        time: 5,
+        hide: false,
+        animation_duration: 0.2,
+        showCloseIcon: true,
+        newestOnTop: true,
+        debug: true
+    };
+};
+
+notficationJS_getDefaultsStyles = function () {
     return {
-        fontSize: "14px",
-        fontWeight: "600",
-        margin: "",
-        padding: "",
-        width: "",
-        textDecoration: "",
-        textTransform: "uppercase",
-        textAlign: "",
-        background: "",
-        color: ""
-    }
-}
-notficationJS_getDefaultsStylesDescription = function(){
+        maxWidth: '375px',
+        backgroundColor: '#F1F5F6',
+        color: '#242424',
+        border: 'none',
+        padding: '10px 15px',
+        borderRadius: '5px',
+        boxShadow: '0px 1px 9px 4px #242424',
+        marginTop: '15px',
+        width: 'calc(100vw - 80px)',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        transition: '.3s all ease'
+    };
+};
+
+notficationJS_getDefaultsStylesTitle = function () {
     return {
-        fontSize: "14px",
-        fontWeight: "",
-        margin: "5px 0 0 0",
-        padding: "0 0 0 0",
-        width: "",
-        textDecoration: "",
-        textTransform: "",
-        textAlign: "",
-        background: "",
-        color: ""
-    }
-}
-notficationJS_getDefaultsStylesLink = function(){
+        fontSize: '14px',
+        fontWeight: '600',
+        margin: '',
+        padding: '',
+        width: '',
+        textDecoration: '',
+        textTransform: 'uppercase',
+        textAlign: '',
+        background: '',
+        color: ''
+    };
+};
+
+notficationJS_getDefaultsStylesDescription = function () {
     return {
-        fontSize: "",
-        fontWeight: "600",
-        margin: "",
-        padding: "",
-        width: "",
-        textDecoration: "none",
-        textTransform: "",
-        textAlign: "",
-        background: "",
-        color: "inherit"
+        fontSize: '14px',
+        fontWeight: '',
+        margin: '5px 0 0 0',
+        padding: '0 0 0 0',
+        width: '',
+        textDecoration: '',
+        textTransform: '',
+        textAlign: '',
+        background: '',
+        color: ''
+    };
+};
+
+notficationJS_getDefaultsStylesLink = function () {
+    return {
+        fontSize: '',
+        fontWeight: '600',
+        margin: '',
+        padding: '',
+        width: '',
+        textDecoration: 'none',
+        textTransform: '',
+        textAlign: '',
+        background: '',
+        color: 'inherit'
+    };
+};
+
+notficationJS_getDefaultsStylesCloseIcon = function () {
+    return {
+        position: 'absolute',
+        height: '3px',
+        width: '15px',
+        background: '#242424',
+        borderRadius: '3px',
+        margin: '',
+        padding: '',
+        right: '10px',
+        left: '',
+        top: '',
+        bottom: ''
+    };
+};
+
+newNotification = function (notification_user) {
+    if (!notification_user) {
+        console.error(
+            'NotificationJS: You need to set a map with at least a title property like this: '
+        );
+        console.error(
+            'NotificationJS: newNotification({"title":"Your title goes here."})'
+        );
+        return 0;
     }
-}
+    checkErrors(notification_user);
 
-notficationJS_getDefaultsStylesCloseIcon = function(){
-    return({
-        position: "absolute",
-        height: "3px",
-        width: "15px",
-        background: "#242424",
-        borderRadius: "3px",
-        margin: "",
-        padding: "",
-        right: "10px",
-        left: "",
-        top: "",
-        bottom: ""
-    })
-}
+    let notificationStyles = {
+        ...notficationJS_getDefaultsStyles(),
+        ...notification_user.styles
+    };
+
+    let notification = {
+        ...notficationJS_getDefaults(),
+        ...notification_user
+    };
+    console.log(notification);
 
 
-newNotification = function(notification_user){
-    if(!notification_user){
-        console.error("NotificationJS: You need to set a map with at least a title property like this: ")
-        console.error("NotificationJS: newNotification({\"title\":\"Your title goes here.\"})")
-        return 0
-    }
-    checkErrors(notification_user)
-    
-    let notificationStyles = $.extend({},notficationJS_getDefaultsStyles(),notification_user.styles)
-    let notification = $.extend({},notficationJS_getDefaults(),notification_user)
-    let div_father = $(`<div class="notification-panel__item notification__${notification.status}"></div>`)
+    let div_father = document.createElement('div');
 
-    div_father.css(notificationStyles)
 
-    if(notification.status == "error")
-        div_father.css("borderTop","5px solid #c0392b")
-    if(notification.status == "ok")
-        div_father.css("borderTop","5px solid #27ae60")
+    Object.assign(div_father.style, notificationStyles);
+    if (notification.status === 'error')
+        div_father.style.borderTop = '5px solid #c0392b';
+    if (notification.status === 'ok')
+        div_father.style.borderTop = '5px solid #27ae60';
 
     //left part
-    let div_left = $('<div class="notification-left"></div>')
-    let div_title = $(`<div class="notification__title">${notification.title}</div>`)
-    let notificationStylesTitle = $.extend({},notficationJS_getDefaultsStylesTitle(),notification_user.titleStyles)
-    div_title.css(notificationStylesTitle)
-    div_title.appendTo(div_left)
+    let div_left = document.createElement('div');
+    let div_title = document.createElement('div');
+    div_left.innerHTML = notification.title;
+    let notificationStylesTitle = {
+        ...notficationJS_getDefaultsStylesTitle(),
+        ...notification_user.titleStyles
+    };
 
+    Object.assign(div_title.style, notificationStylesTitle);
+    div_left.appendChild(div_title);
 
-    let link = null
+    let link = null;
     if (notification.link) {
         if (notification.link.href) {
-            link = $(`<a class="notification__link" href="${notification.link.href}"></a>`)
+            link = document.createElement('a');
+            link.setAttribute('href', notification.link.href);
             if (notification.link.text == undefined) {
-                link.text(notification.link.href)
-            }
-            else {
-                link.text(notification.link.text)
-            }
+                link.innerHTML = notification.link.href;
+                link.text(notification.link.href);
+            } else link.innerHTML = notification.link.text;
         }
     }
 
     if (notification.description) {
-        let description
+        let description;
         if (link != null) {
-            let notificationStylesLink = $.extend({},notficationJS_getDefaultsStylesLink(),notification_user.linkStyles)
-            link.css(notificationStylesLink)
-            description = $(`<div class="notification__description">${notification.description} </div>`)
-            link.appendTo(description)
+            let notificationStylesLink = {
+                ...notficationJS_getDefaultsStylesLink,
+                ...notification_user.linkStyles
+            };
+            Object.assign(link.style, notificationStylesLink);
+            description = document.createElement('div');
+            description.innerHTML = notification.description;
+            description.append(link);
+        } else {
+            description = document.createElement('div');
+            description.innerHTML = notification.description;
         }
-        else {
-            description = $(`<div class="notification__description">${notification.description}</div>`)
-        }
-        let notificationStylesDescription = $.extend({},notficationJS_getDefaultsStylesDescription(),notification_user.descriptionStyles)
-        description.css(notificationStylesDescription)
-        description.appendTo(div_left)
-    }
-    else {
-        if (link != null)
-            link.appendTo(div_left)
-    }
+        let notificationStylesDescription = {
+            ...notficationJS_getDefaultsStylesDescription(),
+            ...notification_user.descriptionStyles
+        };
+        Object.assign(description.style, notificationStylesDescription);
+        div_left.append(description)
+    } else if (link != null) div_left.append(link);
 
-
-    div_father.append(div_left)
+    div_father.append(div_left);
 
     //right part
     if (notification.showCloseIcon) {
-        let div_right = $('<div class="notification-right"></div>')
-        let close_icon_css = $.extend({},notficationJS_getDefaultsStylesCloseIcon(),notification_user.closeIconStyles)
-        let close_icon = $('<div></div>')
-        let close_icon_bar1 = $('<div></div>').css(close_icon_css)
-        let close_icon_bar2 = $('<div></div>').css(close_icon_css)
-        close_icon
-            .append(close_icon_bar1.css("transform","rotate(45deg)"))
-            .append(close_icon_bar2.css("transform","rotate(-45deg)"))
+        let div_right = document.createElement('div');
+        let close_icon_css = {
+            ...notficationJS_getDefaultsStylesCloseIcon(),
+            ...notification_user.closeIconStyles
+        };
+        let close_icon = document.createElement('div');
+        let close_icon_bar1 = document.createElement('div');
+        let close_icon_bar2 = document.createElement('div');
+        Object.assign(close_icon_bar1.style, close_icon_css);
+        Object.assign(close_icon_bar2.style, close_icon_css);
+        close_icon_bar1.style.transform = 'rotate(45deg)';
+        close_icon_bar2.style.transform = 'rotate(-45deg)';
+        close_icon.append(close_icon_bar1);
+        close_icon.append(close_icon_bar2);
 
-        close_icon.click(function() {
-            div_father.fadeOut(anim_t)
-            setTimeout(function() {
-                div_father.remove()
-            }, anim_t + 100)
-        })
-        close_icon.appendTo(div_right)
-        div_right.css({
-            "margin": "0 0 0 15px",
-            "cursor": "pointer"
-        })
-        div_father.append(div_right)
+        close_icon.addEventListener('click', () => {
+            div_father.style.opacity = 0;
+            setTimeout(function () {
+                div_father.remove();
+            }, anim_t + 100);
+        });
+        div_right.append(close_icon);
+        Object.assign(div_right.style, {
+            margin: '0 0 0 15px',
+            cursor: 'pointer'
+        });
+        div_father.append(div_right);
     }
 
     //Show
+    let notification_panel;
+    if (document.querySelector('.notification-panel') == null) {
+        notification_panel = document.createElement('div');
+        notification_panel.classList.add('notification-panel');
+        let body = document.getElementsByTagName('body')[0];
+        body.append(notification_panel);
+    } else notification_panel = document.querySelector('.notification-panel');
 
-    if (!($('.notification-panel').length)) {
-        let notification_panel = $('<div class="notification-panel"></div>')
-        $('body').append(notification_panel)
-    }
-
-    let anim_t = notification.animation_duration * 1000
-    if (notification.newestOnTop) {
-        $('.notification-panel').prepend(div_father).fadeOut(0).fadeIn(anim_t)
-    }
-    else {
-        $('.notification-panel').append(div_father).fadeOut(0).fadeIn(anim_t)
-    }
+    let anim_t = notification.animation_duration * 1000;
+    notification.newestOnTop ?
+        notification_panel.prepend(div_father) :
+        notification_panel.append(div_father);
 
     if (notification.hide) {
-        let display_t = notification.time * 1000
-        //Hide            
-        setTimeout(function() {
-            div_father.fadeOut(anim_t)
-        }, display_t)
+        let display_t = notification.time * 1000;
+        //Hide
+        setTimeout(function () {
+            div_father.style.opacity = 0;
+            div_father.style.transform = 'scale(0)';
+        }, display_t);
 
         //destroy
-        setTimeout(function() {
-            div_father.remove()
-        }, display_t + anim_t + 100)
+        setTimeout(function () {
+            div_father.remove();
+        }, display_t + anim_t + 100);
     }
-
-}
+};
